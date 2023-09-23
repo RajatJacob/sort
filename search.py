e0ea3dc9c3c0edc57431eec724b1e968c844aafd
Rajat Jacob <rajatjacob@gmail.com>
2023-09-24 00:29:21 +0530

    Copied base class to `search.py`

--- search.py
+++ search.py
@@ -1,1 +1,26 @@
 
+from abc import ABC, abstractmethod
+
+"""Module with the base implementation of a Sort class."""
+
+
+class Search(ABC):
+    """Abstract base class for searching."""
+
+    def __init__(self, items):
+        self._items = items
+
+    @abstractmethod
+    def _search(self):
+        """Returns the index of the element to be searched
+        Returns:
+                The index of the element to be searched or -1 if not found
+        """
+        pass
+
+    def get_items(self):
+        return self._items
+
+    def _time(self):
+        self.time = 0
+        return self.time
