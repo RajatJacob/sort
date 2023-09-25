8983a9f6852aac49f2888cdeaff8fbf645638b5e
saral7293 <145864657+saral7293@users.noreply.github.com>
2023-09-25 15:55:28 +0530

    Update sort.py

--- sort.py
+++ sort.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-
-"""Module with the base implementation of a Sort class."""
+import time
+import random
+import matplotlib.pyplot as plt
 
 class Sort(ABC):
     """Abstract base class for sorting."""
@@ -16,69 +17,96 @@ class Sort(ABC):
             List: The sorted elements.
         """
         pass
-#hello testing how to make changes and get them to reflect 
+
     def get_items(self):
         return self._items
 
     def _time(self):
         self.time = 0
         return self.time
-    
-    
-    class MergeSort(Sort):
+
+class MergeSort(Sort):
     """Class that represents a MergeSort implementation."""
 
     def _sort(self):
-    # Check if the length of the list is greater than 1 (base case for recursion)
-    if len(self._items) > 1:
-        # Calculate the midpoint of the list
-        mid = len(self._items) // 2
+        try:
+            # Check if the length of the list is greater than 1 (base case for recursion)
+            if len(self._items) > 1:
+                # Calculate the midpoint of the list
+                mid = len(self._items) // 2
 
-        # Divide the list into two halves: L and R
-        L = self._items[:mid]
-        R = self._items[mid:]
+                # Divide the list into two halves: L and R
+                L = self._items[:mid]
+                R = self._items[mid:]
 
-        # Create instances of MergeSort for left and right halves
-        left_sort = MergeSort(L)
-        left_sort._sort()  # Recursively sort the left half
-        right_sort = MergeSort(R)
-        right_sort._sort()  # Recursively sort the right half
+                # Create instances of MergeSort for left and right halves
+                left_sort = MergeSort(L)
+                left_sort._sort()  # Recursively sort the left half
+                right_sort = MergeSort(R)
+                right_sort._sort()  # Recursively sort the right half
 
-        # Merge the two sorted halves back into the original list
-        self._merge(L, R)
+                # Merge the two sorted halves back into the original list
+                self._merge(L, R)
+                # Return the sorted list
+                return self._items
+        except RecursionError as e:
+            # Handle recursion depth exceeded error
+            print("Recursion depth exceeded. Consider using a shorter array")
+        except TypeError as e:
+            # Handle type error
+            print("Input list is of incompatible type.")
+        except IndexError as e:
+            # Handle index error
+            print("Index error. Check list indices.")
+        except Exception as e:
+            # Handle other exceptions
+            print(f"An unexpected error occurred: {str(e)}")
 
-    # Return the sorted list
-    return self._items
+    def _merge(self, left, right):
+        i = j = k = 0
 
+        try:
+            # Merge the two halves (left and right) into a single sorted list
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    self._items[k] = left[i]
+                    i += 1
+                else:
+                    self._items[k] = right[j]
+                    j += 1
+                k += 1
 
-def _merge(self, left, right):
-    i = j = k = 0
+            # Check for any remaining elements in left and right
+            while i < len(left):
+                self._items[k] = left[i]
+                i += 1
+                k += 1
+            while j < len(right):
+                self._items[k] = right[j]
+                j += 1
+                k += 1
 
-    # Merge the two halves (left and right) into a single sorted list
-    while i < len(left) and j < len(right):
-        if left[i] <= right[j]:
-            self._items[k] = left[i]
-            i += 1
-        else:
-            self._items[k] = right[j]
-            j += 1
-        k += 1
+        except IndexError as e:
+            # Handle index error
+            print("Index error. Check list indices.")
+        except TypeError as e:
+            # Handle type error
+            print("Input lists contain incompatible elements.")
+        except Exception as e:
+            # Handle other unexpected exceptions
+            print(f"An unexpected error occurred: {str(e)}")
 
-    # Check for any remaining elements in L and R
-    while i < len(left):
-        self._items[k] = left[i]
-        i += 1
-        k += 1
-    while j < len(right):
-        self._items[k] = right[j]
-        j += 1
-        k += 1
-
-
-def _time(self):
-    # Measure the execution time of the sorting process
-    start = time.time()  # Record the start time
-    self._sort()         # Call the sorting algorithm
-    end = time.time()    # Record the end time
-    self.time = end - start  # Calculate and store the elapsed time
-    return self.time
+    def _time(self):
+        try:
+            # Measure the execution time of the sorting process
+            start = time.time()  # Record the start time
+            self._sort()         # Call the sorting algorithm
+            end = time.time()    # Record the end time
+            self.time = end - start  # Calculate and store the elapsed time
+            return self.time
+        except TypeError as e:
+            # Handle type error
+            print("Sorting algorithm encountered a TypeError.")
+        except Exception as e:
+            # Handle other unexpected exceptions
+            print(f"An unexpected error occurred: {str(e)}")
