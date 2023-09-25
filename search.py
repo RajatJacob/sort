ed1416bbd5e6d76a3bbe2c340bd4982bc03f2adf
amalunnikrishnan <131914900+amalunnikrishnan@users.noreply.github.com>
2023-09-26 00:25:19 +0530

    Added the LinSearch Thing (#2)
    * Added the LinSearch Thing
    
    * Did'nt do anything, probably added a space
    
    * Binary search (#3)
    
    * Added `target` property to Search ABC (#1)
    
    How can one doubt the absolute prowess of RJ the GOAT?
    
    * Added binary search
    
    ---------
    
    Co-authored-by: amalunnikrishnan <131914900+amalunnikrishnan@users.noreply.github.com>
    
    * Bugfix
    
    * Fixed everything as requested
    
    * imported time
    
    ---------
    
    Co-authored-by: Rajat Jacob <rajatjacob@gmail.com>

--- search.py
+++ search.py
@@ -1,6 +1,6 @@
 
 from abc import ABC, abstractmethod
-
+import time
 """Module with the base implementation of a Sort class."""
 
 
@@ -8,12 +8,13 @@ class Search(ABC):
     """Abstract base class for searching."""
 
     def __init__(self, items, target):
-        self._items = items
+
+        self._items = list(items)
         self._target = target
 
     @abstractmethod
     def _search(self):
-        """Returns the index of the element to be searched
+        """ Returns the index of the element to be searched
         Returns:
                 The index of the element to be searched or -1 if not found
         """
@@ -28,3 +29,55 @@ class Search(ABC):
     def _time(self):
         self.time = 0
         return self.time
+
+
+
+class BinarySearch(Search):
+
+    def _search(self):
+        start_time = time.time()
+        i = list(sorted(self._items))
+        a = 0
+        b = len(i) - 1
+        while a <= b:
+            mid = (a + b) // 2
+            if i[mid] < self._target:
+                a = mid + 1
+            elif i[mid] > self._target:
+                b = mid - 1
+            else:
+                self.time = time.time() - start_time
+                return mid
+        self.time = time.time() - start_time
+        return -1
+
+
+class LinSearch(Search):
+    def _search(self):
+        """
+        Perform linear search to find the index of the target in the list.
+
+        Returns:
+            int: The index of the target element, or -1 if not found.
+        """
+        start_time = time.time() 
+        try:
+            items = self.get_items()  # Get the list of items to search within
+            target = self.get_target()  # Get the target value to search for
+            for i in range(len(items)):
+                if items[i] == target:  # Check if the current item matches the target
+                    return i  # Return the index if found
+                self.time = time.time() - start_time
+            self.time = time.time() - start_time
+            return -1  # Return -1 if the target is not found
+
+        except TypeError:
+            # Handle the case where items is not a list or target is not a comparable value
+            raise TypeError(
+                "Items should be a list, and target should be a comparable value.")
+
+        except Exception as e:
+            # Handle other exceptions by re-raising them for further investigation
+            raise e
+
+
